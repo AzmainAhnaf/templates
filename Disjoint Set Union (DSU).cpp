c77989dbfb7af4ae786c4906908cc5961de38b8b
Azmain Ahnaf <azmainahnaf2003@gmail.com>
2025-09-12 14:13:30 +0600

    Update Disjoint Set Union (DSU).cpp

--- Disjoint Set Union (DSU).cpp
+++ Disjoint Set Union (DSU).cpp
@@ -1,65 +1,31 @@
-// Problem Name: UnionFind
+// Problem Name: Unionfind
 // Problem Link: https://judge.yosupo.jp/problem/unionfind
 
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
  
 using namespace std;
-using namespace __gnu_pbds;
- 
-// Macros
-#define REP(i, j, n) for(int i = j; i < n; i++)
-#define trav(i, a) for(auto i: a)
-#define all(x) x.begin(), x.end()
-#define PB push_back
-#define EB emplace_back
-#define MP make_pair
-#define F first
-#define S second
-#define GCD __gcd
-#define DEBUG(i) cout << "DEBUG " << i << "\n";
-#define CASE(i) cout << "Case " << i << ": ";
-#define MEM(arr, val) memset(arr, val, sizeof(arr));
  
 // Type Names
-typedef unsigned long long ull;
 typedef long long ll;
 typedef vector<int> vi;
-typedef vector<long long> vll;
-typedef pair<int, int> pii;
-typedef long double ld;
-typedef tree<int,null_type,less<int>,rb_tree_tag,
-tree_order_statistics_node_update> indexed_set;
-typedef tree<int, null_type, less_equal<int>, rb_tree_tag, 
-tree_order_statistics_node_update> ordered_multiset;
  
-// functions    
-template <typename T>
-void pv(vector<T> &a){
-    for (auto elem: a){
-        cout << elem << " ";
-    }
-    cout << "\n";
-}
-
-// Global Variables
-const ll MOD = 1e9 + 7;
-const ll mod = 998244353;
-const ll INF = 1e18;
-
+// if there are n nodes 1 indexed then use DSU(n) in constructor
+// if there are n nodes 0 indexed then using DSU(n - 1) is sufficient
 struct DSU {
 
-    vector<int> p, size;
+    vi p, size;
 
     DSU(int n){
         p.resize(n + 1);
-        size.resize(n + 1, 1);
         for (int i = 1; i <= n; i++) p[i] = i;
+        size.assign(n + 1, 1);
     }
 
     int get(int a){
-        if (a == p[a]) return a;
-        return p[a] = get(p[a]);
+        if (a != p[a]){
+            p[a] = get(p[a]);
+        }
+        return p[a];
     }
 
     void merge(int a, int b){
@@ -67,7 +33,7 @@ struct DSU {
         b = get(b);
         if (a != b){
             if (size[a] < size[b]) swap(a, b);
-            p[b] = p[a];
+            p[b] = a;
             size[a] += size[b];
         }
     }
@@ -77,7 +43,9 @@ struct DSU {
 void solve(int tc){
     int n, q;
     cin >> n >> q;
-    DSU dsu(n);
+
+    // as nodes are 0-indexed
+    DSU dsu(n - 1);
 
     while(q--){
         int t, u, v;
