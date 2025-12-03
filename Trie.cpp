8f98018245f872b20e06a580819692853850f755
Azmain Ahnaf <87205879+AzmainAhnaf@users.noreply.github.com>
2025-12-03 19:46:39 +0600

    Update Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -2,53 +2,11 @@
 // Problem Link: https://www.spoj.com/problems/ADAINDEX/
 
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
  
 using namespace std;
-using namespace __gnu_pbds;
  
 // Macros
 #define REP(i, j, n) for(int i = j; i < n; i++)
-#define trav(i, a) for(auto i: a)
-#define all(x) x.begin(), x.end()
-#define PB push_back
-#define EB emplace_back
-#define MP make_pair
-#define F first
-#define S second
-#define GCD __gcd
-#define DEBUG(i) cout << "DEBUG " << i << "\n";
-#define CASE(i) cout << "Case #" << i << ": ";
-#define MEM(arr, val) memset(arr, val, sizeof(arr));
- 
-// Type Names
-typedef unsigned long long ull;
-typedef long long ll;
-typedef vector<int> vi;
-typedef vector<long long> vll;
-typedef pair<int, int> pii;
-typedef pair<long long, long long> pll;
-typedef pair<long double, long double> pld;
-typedef long double ld;
-typedef tree<int,null_type,less<int>,rb_tree_tag,
-tree_order_statistics_node_update> indexed_set;
-typedef tree<int, null_type, less_equal<int>, rb_tree_tag, 
-tree_order_statistics_node_update> ordered_multiset;
- 
-// functions    
-template <typename T>
-void pv(vector<T> &a){
-    for (auto elem: a){
-        cout << elem << " ";
-    }
-    cout << "\n";
-}
-
-// Global Variables
-const ll MOD = 1e9 + 7;
-const ll mod = 998244353;
-const ll INF = 1e18;
-const ld EPS = 1e-6;
 
 struct TrieNode {
 
