208ecf80d521d42dda0dce8dc0195e0448b6b0fe
Azmain Ahnaf <87205879+AzmainAhnaf@users.noreply.github.com>
2026-05-31 21:52:12 +0600

    Update Dinic (Max Flow).cpp

--- Dinic (Max Flow).cpp
+++ Dinic (Max Flow).cpp
@@ -35,13 +35,13 @@ const ll INF = 1e18;
 const int inf = 1e9;
 
 struct FlowEdge {
-    int v, u;
-    int cap, flow = 0;
-    FlowEdge(int v, int u, ll cap) : v(v), u(u), cap(cap) {}
+    int u, v;
+    ll cap, flow = 0;
+    FlowEdge(int u, int v, ll cap): u(u), v(v), cap(cap) {}
 };
 
 struct Dinic {
-    const int flow_inf = 1e9;
+    const ll flow_inf = 1e18;
     vector<FlowEdge> edges;
     vector<vi> adj;
     int n, m = 0;
@@ -49,45 +49,46 @@ struct Dinic {
     vi level, ptr;
     queue<int> q;
 
-    Dinic(int n, int s, int t) : n(n), s(s), t(t) {
+    Dinic(int n, int s, int t): n(n), s(s), t(t) {
         adj.resize(n + 1);
         level.resize(n + 1);
         ptr.resize(n + 1);
     }
 
-    void add_edge(int v, int u, ll cap){
-        edges.emplace_back(v, u, cap);
-        edges.emplace_back(u, v, 0ll);
-        adj[v].PB(m);
-        adj[u].PB(m + 1);
+    void add_edge(int u, int v, ll cap) {
+        edges.emplace_back(u, v, cap);
+        edges.emplace_back(v, u, 0ll);
+        adj[u].PB(m);
+        adj[v].PB(m + 1);
         m += 2;
     }
 
-    bool bfs(){
-        while (!q.empty()){
-            int v = q.front();
+    bool bfs() {
+        while (!q.empty()) {
+            int u = q.front();
             q.pop();
-            for (int id: adj[v]){
+            for (int id: adj[u]) {
                 if (edges[id].cap == edges[id].flow) continue;
-                if (level[edges[id].u] != -1) continue;
-                level[edges[id].u] = level[v] + 1;
-                q.push(edges[id].u);
+                if (level[edges[id].v] != -1) continue;
+                level[edges[id].v] = level[u] + 1;
+                q.push(edges[id].v);
             }
         }
         return level[t] != -1;
     }
 
-    int dfs(int v, int pushed) {
-        if (pushed == 0)
+    ll dfs(int u, ll pushed) {
+        if (pushed == 0) {
             return 0;
-        if (v == t){
+        }
+        if (u == t) {
             return pushed;
         }
-        for (int &cid = ptr[v]; cid < (int)adj[v].size(); cid++){
-            int id = adj[v][cid];
-            int u = edges[id].u;
-            if (level[v] + 1 != level[u]) continue;
-            int tr = dfs(u, min(pushed, edges[id].cap - edges[id].flow));
+        for (int &cid = ptr[u]; cid < (int)adj[u].size(); cid++) {
+            int id = adj[u][cid];
+            int v = edges[id].v;
+            if (level[u] + 1 != level[v]) continue;
+            ll tr = dfs(v, min(pushed, edges[id].cap - edges[id].flow));
             if (tr == 0) continue;
             edges[id].flow += tr;
             edges[id ^ 1].flow -= tr;
@@ -96,7 +97,7 @@ struct Dinic {
         return 0;
     }
 
-    ll flow(){
+    ll flow() {
         ll f = 0;
         while (true) {
             fill(level.begin(), level.end(), -1);
@@ -104,7 +105,7 @@ struct Dinic {
             q.push(s);
             if (!bfs()) break;
             fill(ptr.begin(), ptr.end(), 0);
-            while (ll pushed = dfs(s, flow_inf)){
+            while (ll pushed = dfs(s, flow_inf)) {
                 f += pushed;
             }
         }
@@ -127,8 +128,6 @@ void solve(int tst){
     }
 
     cout << dinic.flow() << "\n";
-    
-
 }
 
 signed main(){
